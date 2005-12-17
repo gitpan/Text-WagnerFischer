f3ed29590989ac1e5b3f376e65bfb4087ce42198
Davide Bergamini <davidebe75@yahoo.it>
2005-12-17 17:25:02 +0000

    Import of DAVIDEBE/Text-WagnerFischer-0.05 from CPAN.
    gitpan-cpan-distribution: Text-WagnerFischer
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         DAVIDEBE/Text-WagnerFischer-0.05.tar.gz
    gitpan-cpan-author:       DAVIDEBE
    gitpan-cpan-maturity:     released

--- WagnerFischer.pm
+++ WagnerFischer.pm
@@ -4,7 +4,7 @@ use strict;
 use Exporter;
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $REFC);
 
-$VERSION     = '0.04';
+$VERSION     = '0.05';
 @ISA         = qw(Exporter);
 @EXPORT      = ();
 @EXPORT_OK   = qw(&distance);
